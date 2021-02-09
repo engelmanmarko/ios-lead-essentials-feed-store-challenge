d871eb10e93be31e470faf31255f1e23f274d404
Marko Engelman <engleman.marko@gmail.com>
2021-02-09 12:41:17 +0100

    Add 'RealmFeedStore' and conform to 'FeedStore'.

---
+++ FeedStoreChallenge/Infrastructure/RealmFeedStore.swift
@@ -0,0 +1,25 @@
+//
+//  RealmFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Marko Engelman on 09/02/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public final class RealmFeedStore: FeedStore {
+	public init() { }
+	
+	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+		
+	}
+	
+	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+		
+	}
+	
+	public func retrieve(completion: @escaping RetrievalCompletion) {
+		
+	}
+}
